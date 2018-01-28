67ab337ee33a01661f381b4bd116acc9c863c7b1
shizekang <shizekang1@gmail.com>
2018-01-28 09:52:35 +0800

    test!!!2018-1-27 shizekang

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,11 +1,17 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Test1 {
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis());
+        Map<String, Object> map = new HashMap<String, Object>();
+        Iterator<Map.Entry<String, Object>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry entry = iterator.next();
+            String key = (String) entry.getKey();
 
-
-        Integer h = new Integer(3);
-
-        Integer h1 = new Integer(34);
+        }
 
     }
 }
